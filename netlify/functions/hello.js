1865e241eefbbf4cbfdbb52897ccb3ad0cef36a6
Only1Apache <Krisja678@gmail.com>
2025-08-11 03:16:42 -0600

    Delete netlify directory

--- netlify/functions/hello.js
+++
@@ -1,8 +0,0 @@
-// /.netlify/functions/hello
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json", "cache-control": "no-store", "access-control-allow-origin": "*" },
-    body: JSON.stringify({ ok: true, msg: "Functions are working 🎉" })
-  };
-};
